c79562444ef7c3834e3a4795574b0488465ca951
Akubo James Andrew <Jamesalone3569@gmail.com>
2022-07-15 08:17:14 -0700

    function that prints an integer

--- 0x06-pointers_arrays_strings/main.h
+++ 0x06-pointers_arrays_strings/main.h
@@ -20,9 +20,6 @@ void print_numbers(void);
 void print_most_numbers(void);
 void more_numbers(void);
 void print_line(int n);
-void print_diagonal(int n);
-void print_square(int size);
-void print_triangle(int size);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
 void _puts(char *str);
@@ -42,4 +39,8 @@ char *string_toupper(char *);
 char *cap_string(char *);
 char *leet(char *);
 char *rot13(char *);
+void print_number(int n);
+char *infinite_add(char *n1, char *n2, char *r, int size_r);
+void print_buffer(char *b, int size);
+
 #endif /* _MAIN_H_ */
