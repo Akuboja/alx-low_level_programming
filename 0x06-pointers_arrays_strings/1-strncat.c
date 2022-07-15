4023a5e87ebdb154bff677bf85a64ba945689bde
Akubo James Andrew <Jamesalone3569@gmail.com>
2022-07-15 07:53:13 -0700

    function that copies a string

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -1,10 +1,10 @@
 #include "main.h"
 
 /**
- * *_strncat - concatenates two strings
+ *_strncat - concatenates two strings
  *@dest: destination
  *@src: source
- @n: number of bytes
+ *@n: number of bytes
  * Return: pointer to the destination
  */
 
@@ -12,7 +12,7 @@ char *_strncat(char *dest, char *src, int n)
 {
 	int i;
 
-	for(i = 0; i < n && src[i] != '\0'; i++)
+	for (i = 0; i < n && src[i] != '\0'; i++)
 		dest[i] = src[i];
 	for ( ; i < n; i++)
 	{
